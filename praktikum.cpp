2fb9b6dab0c5f804616336394d3c3b68339f1685
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-04-13 10:42:28 +0700

    menambahkan Void linearsearch

--- praktikum.cpp
+++ praktikum.cpp
@@ -19,6 +19,54 @@ void input()
 			cout <<"\nArray should have minimun 1 and maximum 20 elements.\n\n";
 	}
 
-	// Array 
+	// Array array elements 
+	cout << "\n______________________\n";
+	cout << " Enter array elements \n";
+	cout << "______________________\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + i) << "> ";
+		cin >> arr[i];
+	}
 
+}
+
+
+void LinearSearch()
+{
+	char ch;
+	int comparison; //Number of comparisons
+
+	do              // Langkah 3
+	{
+		// Accept the number to be searched
+		cout << "\nEnter the element you want to search: "; // Langkah 1
+		int item;
+		cin >> item;
+
+		comparison = 0;
+		for (i = 0; i < n; i++)                         //Langkah 2 dan 4
+		{
+			comparison++;
+			if (arr[i] == item)
+			{                                       //Langkah 5 a found 
+				cout << "\n" << item << " found at position " << (i + i) << endl;
+				break;
+			}
+		}
+
+		if (i == n)
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparison: " << comparison << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
+}
+
+
+int main()
+{
+	input();
+	LinearSearch();
 }
